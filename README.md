534d1ac8eceeb2064cbaaeadfb366cce494446e3
Mostafa Samer <mostafa@debian>
2019-05-02 03:56:18 +0200

    fix login

--- README.md
+++ README.md
@@ -8,3 +8,19 @@
     <li>node-couchdb</li>
     <li>path</li>
 </ul>
+<h4>Database info</h4>
+<ul>
+    <li>Doc:
+        {
+  "_id": "_design/view-all",
+  "_rev": "3-cc81c32cdf5d0e1005c5fb5fdd1da31b",
+  "views": {
+    "all-index": {
+      "map": "function (doc) {\n  emit(doc.email, doc.pass, {\n  \"name\": \"Mostafa\",\n  \"email\": \"mostafa@email.com\",\n  \"pass\": \"1234\",\n  \"phone\": \"012\",\n  \"address\": \"24 El-fairouz\"\n  });\n}"
+    }
+  },
+  "language": "javascript"
+}
+    </li>
+    <li>view: _design/all_data/_view/new-view</li>
+</ul>
--- model/data.js
+++ model/data.js
@@ -1,4 +1,5 @@
 const nodeCouchdb = require('node-couchdb');
+const path = require('path');
 
 const couch = new nodeCouchdb({
     auth:{
@@ -8,7 +9,7 @@ const couch = new nodeCouchdb({
 
 });
 const dbName = 'login_reg';
-const viewUrl = '_design/all_data/_view/new-view';
+const viewUrl = '_design/view-all/_view/all-index';
 
 var insert_data = function(data) {
     //console.log("Data: " + data.name);
@@ -17,7 +18,7 @@ var insert_data = function(data) {
         couch.insert(dbName, {
             name: data.name,
             email: data.email,
-            password: data.pass,
+            pass: data.pass,
             phone: data.phone,
             address: data.address
         }).then(
@@ -34,8 +35,21 @@ var insert_data = function(data) {
 var check_data = function(data, callback) {
     const email = data.email;
     const pass = data.pass;
-    //search for data
-    // return id if found in [callback(id);]
+    couch.get(dbName, viewUrl + "?key=\"" + email + "\"").then(
+        function(data, headers, status) {
+            //data.data.rows[0].key     **email**
+            //data.data.rows[0].value   **pass**
+            if (email == data.data.rows[0].key
+                && pass == data.data.rows[0].value) {
+                    console.log("Correct");
+            } else {
+                console.log("Wrong");
+            }
+        },
+        function(err) {
+            console.log("Error");
+        }
+    )
 }
 
 module.exports = {
